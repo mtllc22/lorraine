b3a9b44783d3a8e7075263d1919dfe9ea91ac144
mtllc22 <21219779+mtllc22@users.noreply.github.com>
2024-11-15 13:25:34 -0300

    alfabetica

--- script.js
+++ script.js
@@ -1,59 +1,69 @@
 function gerarProdutos() {
   const produtosSection = document.getElementById('produtos');
+
+  // Array para armazenar os nomes das imagens
+  const imagens = [];
   
+  // Criar nomes e armazenar no array
   for (let i = 1; i <= 50; i++) {
-    const caminhoImagem = `img/produto (${i}).jpg`;  
+    const caminhoImagem = `img/produto (${i}).jpg`;
+    const nomeImagem = `produto (${i})`.replace(/[()]/g, '');
+    imagens.push({ caminho: caminhoImagem, nome: nomeImagem });
+  }
+
+  // Ordenar as imagens em ordem alfabética
+  imagens.sort((a, b) => a.nome.localeCompare(b.nome, 'pt-BR', { numeric: true }));
+
+  // Criar os elementos no DOM com base na ordem
+  imagens.forEach(imagem => {
     const img = new Image();
-    img.src = caminhoImagem;
+    img.src = imagem.caminho;
     const imgPromise = new Promise((resolve, reject) => {
-      img.onload = function() {
-        resolve(caminhoImagem);  
+      img.onload = function () {
+        resolve(imagem);
       };
-      img.onerror = function() {
-        reject();  
+      img.onerror = function () {
+        reject();
       };
     });
 
-    
     imgPromise
-      .then(() => {
-        const nomeImagem = `produto (${i})`.replace(/[()]/g, '');  
+      .then(({ caminho, nome }) => {
         const divProduto = document.createElement('div');
         divProduto.classList.add('produto');
-        img.alt = nomeImagem;  
+        img.alt = nome;
         divProduto.appendChild(img);
         const nomeImagemElement = document.createElement('p');
-        nomeImagemElement.textContent = nomeImagem;  
+        nomeImagemElement.textContent = nome;
         divProduto.appendChild(nomeImagemElement);
         produtosSection.appendChild(divProduto);
         img.addEventListener('click', () => {
-          abrirModal(caminhoImagem, nomeImagem);
+          abrirModal(caminho, nome);
         });
       })
       .catch(() => {
+        console.warn(`Erro ao carregar a imagem: ${imagem.caminho}`);
       });
-  }
+  });
 }
-
 
 function abrirModal(imagem, nomeImagem) {
   const modal = document.getElementById("modal");
   const modalImg = document.getElementById("modal-img");
   const captionText = document.getElementById("modal-caption");
-  modal.style.display = "block";  
-  modalImg.src = imagem;  
-  captionText.textContent = nomeImagem;  
+  modal.style.display = "block";
+  modalImg.src = imagem;
+  captionText.textContent = nomeImagem;
   const closeBtn = document.getElementById("close");
-  closeBtn.onclick = function() {
+  closeBtn.onclick = function () {
     modal.style.display = "none";
-  }
+  };
 
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = "none";
     }
-  }
+  };
 }
-
 
 gerarProdutos();
